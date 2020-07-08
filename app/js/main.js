6e6668efa7d484923ddcadef2bc725cad6b7d0bd
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-07-08 17:48:09 +0300

    added product-slider added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
-$(function(){
+$(function () {
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+  });
 
-  var mixer = mixitup('.products__inner-box');
+  var mixer = mixitup(".products__inner-box");
+  $('.product-slider__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false,
 
+  });
 });
